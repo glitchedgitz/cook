8e8fedab5dcbaf93272cc9b5c05102ab8d081cb6
LM <lalvarezt89@gmail.com>
2025-11-17 09:33:19 +0100

    Add -no-color flag and NO_COLOR environment variable support

--- v2/pkg/util/cmd.go
+++ v2/pkg/util/cmd.go
@@ -9,5 +9,9 @@ func TerminalColor(text, color string) string {
 }
 
 func TerminalLink(url, text, color string) string {
+	// When colors are disabled (Reset is empty), skip hyperlink escape sequences
+	if Reset == "" {
+		return text
+	}
 	return fmt.Sprintf("%s\033]8;;%s\033\\%s\033]8;;\033\\%s", color, url, text, Reset)
 }
--- v2/pkg/util/colors.go
+++ v2/pkg/util/colors.go
@@ -1,5 +1,7 @@
 package util
 
+import "os"
+
 // These are messed up, something is something else
 var (
 	Blue       = "\u001b[38;5;45m"
@@ -10,3 +12,16 @@ var (
 	Reset      = "\u001b[0m"
 	Reverse    = "\u001b[7m"
 )
+
+// InitColors disables colors if --no-color flag or NO_COLOR env var is set
+func InitColors(noColor bool) {
+	if noColor || os.Getenv("NO_COLOR") != "" {
+		Blue = ""
+		Grey = ""
+		Red = ""
+		White = ""
+		Background = ""
+		Reset = ""
+		Reverse = ""
+	}
+}
